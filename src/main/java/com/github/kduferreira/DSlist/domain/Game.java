a6b198d15cb0d59a4ccac8dbc0468d61b0b88b70
kdueferreira <110642509+kduferreira@users.noreply.github.com>
2023-08-14 14:38:34 -0300

    Game seed

--- src/main/java/com/github/kduferreira/DSlist/domain/Game.java
+++ src/main/java/com/github/kduferreira/DSlist/domain/Game.java
@@ -7,7 +7,7 @@ import java.util.Objects;
 
 @Entity
 @Data
-@Table(name = "tb_game/")
+@Table(name = "tb_game")
 public class Game {
 
     @Id
@@ -18,21 +18,23 @@ public class Game {
     private Integer year;
     private String genre;
 
-    private String plataforms;
+    private String platforms;
     private Double score;
     private String imgUrl;
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game() {
     }
 
-    public Game(Long id, String title, Integer year, String genre, String plataforms, Double score, String imgUrl, String shortDescription, String longDescription) {
+    public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl, String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
-        this.plataforms = plataforms;
+        this.platforms = platforms;
         this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
